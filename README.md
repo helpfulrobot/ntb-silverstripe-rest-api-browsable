38703d0ad32f60fb0724c7c65643f9917a45d917
Christian Blank <christian.blank@haw-hamburg.de>
2016-05-25 21:26:39 +0200

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+OAuth module for Silverstripe REST api
+======================================
+
+[![Build Status](https://travis-ci.org/notthatbad/silverstripe-rest-api-oauth.svg)](https://travis-ci.org/notthatbad/silverstripe-rest-api-oauth)
+[![Latest Stable Version](https://poser.pugx.org/ntb/silverstripe-rest-api-oauth/v/stable)](https://packagist.org/packages/ntb/silverstripe-rest-api-oauth)
+[![License](https://poser.pugx.org/ntb/silverstripe-rest-api-oauth/license)](https://packagist.org/packages/ntb/silverstripe-rest-api-oauth)
+
+This module adds the ability to create a session in the rest api with the help of an OAuth token.
+
+The module supports the following providers:
+
+ * Facebook
+ * Google
+
+## Configuration
+
+The only thing you need to do is to configure the module with your own api id and api secret.
---
+++ code/serializers/BrowsableHtmlSerializer.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Serializer for a browsable html result page.
+ * @author Christian Blank <c.blank@notthatbad.net>
+ */
+class BrowsableHtmlSerializer extends ViewableData implements IRestSerializer {
+
+    /**
+     * @config
+     */
+    private static $is_active = true;
+
+    /**
+     * The content type
+     * @var string
+     */
+    private $contentType = "text/html";
+
+    /**
+     * The given data will be serialized into an html string using a Silverstripe template.
+     *
+     * @param array $data
+     * @return string an html string
+     */
+    public function serialize($data) {
+        $list = $this->recursive($data, 1);
+        return $this->renderWith(['Result', 'Controller'], ['Data' => ArrayList::create($list)]);
+    }
+
+    public function contentType() {
+        return $this->contentType;
+    }
+
+    private function recursive($data, $level) {
+        $list = [];
+        if(is_array($data)) {
+            foreach ($data as $key => $value) {
+                if(is_array($value)) {
+                    $list[] = ArrayData::create(['Key' => $key, 'Value' => '', 'Heading' => true, 'Level' => $level]);
+                    $list = array_merge($list, $this->recursive($value, $level+1));
+                } else {
+                    $list[] = ArrayData::create(['Key' => $key, 'Value' => $value, 'Level' => $level]);
+                }
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * Indicates if the serializer is active.
+     * Serializers can be deactivated to use another implementation for the same mime type.
+     *
+     * @return boolean
+     */
+    public function active() {
+        return $this->config()->get('is_active');
+    }
+}
